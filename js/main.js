dcd51108bf19d8d3d7abb0f4e8b6015388509edf
SlaewSilver <92515705+SlaewSilver@users.noreply.github.com>
2021-11-22 04:00:29 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,28 +1,30 @@
-const NAME = 'Kostya ';
-let surname = 'Yashchenko ';
 const PATRONYMIC_SPECIFIED = false;
-
-console.log ( NAME + surname + ', const( ' + typeof( NAME ) + 
-	' ) variable( ' + typeof( surname ) + ' )');
-console.error ( 'patronymic specified = ' + PATRONYMIC_SPECIFIED + 
-	', const( ' + typeof( PATRONYMIC_SPECIFIED ) + ' )' )
-
 const DATE_OF_BIRTH = 24;
 const MONTH_OF_BIRTH = 03;
 const YEAR_OF_BIRTH = 2001;
+const NAME = 'Kostya ';
 
-console.log ( 'birthday = ( '+ DATE_OF_BIRTH + '.' +  
-	MONTH_OF_BIRTH + '.' + YEAR_OF_BIRTH + ' ) = type_const(' + 
-	typeof( DATE_OF_BIRTH ) + '.' + typeof( MONTH_OF_BIRTH ) + '.' + 
-	typeof( YEAR_OF_BIRTH ) + ')' );
+let surname = 'Yashchenko ';
 
-let date = new Date();
-let years = date.getFullYear() - YEAR_OF_BIRTH;
+console.log (`${NAME} ${surname} const(${typeof( NAME )}) variable(${typeof( surname )})`);
+console.error (`patronymic specified = ${PATRONYMIC_SPECIFIED}, const(${typeof( PATRONYMIC_SPECIFIED )})`);
 
-console.log ( 'years = ' + years + ', type( years ) = ' + typeof( years ) );
+console.log (`birthday = (${DATE_OF_BIRTH}.${MONTH_OF_BIRTH}.${YEAR_OF_BIRTH}) = type_const(${typeof( DATE_OF_BIRTH )}.${typeof( MONTH_OF_BIRTH )}.${typeof( YEAR_OF_BIRTH )})`);
+
+let dates = new Date();
+let years = dates.getFullYear() - YEAR_OF_BIRTH;
+
+console.log (`years = ${years}, type( years ) = ${typeof( years )} `);
+ 
+let obgect_ = {
+	_text: 'snails in the house pets',
+	pets: true,
+	floor: undefined,
+	games: NaN,
+}
 
 let _text = 'snails in the house pets';
 let pets = true;
 
-console.warn ( _text + ' ( _text ) = '  + pets + ' ( pets )' ); 
-console.warn ( 'type( _text ) = ' + typeof( _text ) + ' and type( pets ) = ' + typeof( pets ) );
+console.log(`obgect_ = ${obgect_}`);
+console.log(obgect_);
